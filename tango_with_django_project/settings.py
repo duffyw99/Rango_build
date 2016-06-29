aebf09b11185b786c0e71945223e9e2af2fa5ee2
duffyw99 <Mookyhands@Mookybook.local>
2016-06-29 18:54:57 -0400

    Added 1st app, created view and new app url.py file TODO:Address ROOT_URLCONF issue

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -30,13 +30,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'rango',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango',
+
 )
 
 MIDDLEWARE_CLASSES = (
