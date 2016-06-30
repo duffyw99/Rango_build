680966a573a45654188a970595e019b2144f1379
duffyw99 <mduffyw99@gmail.com>
2016-06-29 22:55:12 -0400

    Added media folders and logic to urls.py that will be used to upload media

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^admin/', admin.site.urls),
     url(r'^rango/', include('rango.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
